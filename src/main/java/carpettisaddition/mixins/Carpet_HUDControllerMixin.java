ec2504b0a7deff5e3f43876fd0d685a654ed22cd
Fallen_Breath <yqx791125833@gmail.com>
2020-06-24 11:54:23 +0800

    cleanup and more epic extension.noop()

--- src/main/java/carpettisaddition/mixins/Carpet_HUDControllerMixin.java
+++ src/main/java/carpettisaddition/mixins/Carpet_HUDControllerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(HUDController.class)
-public class CarpetHUDControllerMixin
+public class Carpet_HUDControllerMixin
 {
 	@Inject(
 			method = "update_hud",
