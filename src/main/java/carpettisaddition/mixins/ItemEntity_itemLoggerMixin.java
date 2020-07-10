0e78ab3bfe10d1410fee4fcd78e30613dbdbf45d
Fallen_Breath <yqx791125833@gmail.com>
2020-07-11 00:09:56 +0800

    log the first event

--- src/main/java/carpettisaddition/mixins/ItemEntity_itemLoggerMixin.java
+++ src/main/java/carpettisaddition/mixins/ItemEntity_itemLoggerMixin.java
@@ -1,8 +1,10 @@
 package carpettisaddition.mixins;
 
 import carpettisaddition.logging.logHelpers.itemLoggerHelper;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +16,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ItemEntity.class)
 public abstract class ItemEntity_itemLoggerMixin
 {
+	private boolean flagDied = false;
+	private boolean flagDespawned = false;
+
 	@Inject(
 			method = "tick",
 			slice = @Slice(
@@ -27,9 +32,13 @@ public abstract class ItemEntity_itemLoggerMixin
 					target = "Lnet/minecraft/entity/ItemEntity;remove()V"
 			)
 	)
-	void onDespawn(CallbackInfo ci)
+	void onDespawned(CallbackInfo ci)
 	{
-		itemLoggerHelper.onItemDespawn((ItemEntity)(Object)this);
+		if (!this.flagDespawned)
+		{
+			itemLoggerHelper.onItemDespawn((ItemEntity) (Object) this);
+			this.flagDespawned = true;
+		}
 	}
 
 	@Inject(
@@ -39,8 +48,12 @@ public abstract class ItemEntity_itemLoggerMixin
 					target = "Lnet/minecraft/entity/ItemEntity;remove()V"
 			)
 	)
-	void onDie(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir)
+	void onDied(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir)
 	{
-		itemLoggerHelper.onItemDie((ItemEntity)(Object)this, source, amount);
+		if (!this.flagDied)
+		{
+			itemLoggerHelper.onItemDie((ItemEntity)(Object)this, source, amount);
+			this.flagDied = true;
+		}
 	}
 }
