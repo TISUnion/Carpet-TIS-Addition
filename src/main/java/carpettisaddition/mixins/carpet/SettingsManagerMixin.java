32598201fc91e17c861f68c704582de534dc33b4
Fallen_Breath <yqx791125833@gmail.com>
2020-08-09 21:35:06 +0800

    better injection
    seems like you can't inject at obfuscated target

--- src/main/java/carpettisaddition/mixins/carpet/SettingsManagerMixin.java
+++ src/main/java/carpettisaddition/mixins/carpet/SettingsManagerMixin.java
@@ -6,20 +6,33 @@ import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static carpet.utils.Translations.tr;
 import static carpettisaddition.CarpetTISAdditionServer.fancyName;
 import static carpettisaddition.CarpetTISAdditionServer.version;
 
+
 @Mixin(SettingsManager.class)
 public class SettingsManagerMixin {
-    @Inject(method = "listAllSettings", at = @At(
-            value = "INVOKE",
-            target = "Lcarpet/settings/SettingsManager;listSettings(Lnet/minecraft/server/command/ServerCommandSource;Ljava/lang/String;Ljava/util/Collection;)I",
-            shift = At.Shift.AFTER
-    ), remap = false)
-    private void printAddonVersion(ServerCommandSource source, CallbackInfoReturnable<Integer> cir) {
+    @Inject(
+            method = "listAllSettings",
+            slice = @Slice(
+                    from = @At(
+                            value = "CONSTANT",
+                            args = "stringValue=ui.version",  // after printed fabric-carpet version
+                            ordinal = 0
+                    )
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcarpet/settings/SettingsManager;getCategories()Ljava/lang/Iterable;",
+                    ordinal = 0
+            ),
+            remap = false
+    )
+    private void printAdditionVersion(ServerCommandSource source, CallbackInfoReturnable<Integer> cir) {
         Messenger.m(source,
                 String.format("g %s ", fancyName),
                 String.format("g %s: ", tr("ui.version",  "version")),
