3e1bcf55276f6214935099d836d309f89c3e30ce
Fallen_Breath <yqx791125833@gmail.com>
2020-08-09 18:12:21 +0800

    opPlayerNoCheat

--- src/main/java/carpettisaddition/CarpetTISAdditionSettings.java
+++ src/main/java/carpettisaddition/CarpetTISAdditionSettings.java
@@ -231,6 +231,13 @@ public class CarpetTISAdditionSettings
 	)
 	public static boolean dispenserNoItemCost = false;
 
+	@Rule(
+			desc = "Disable some command to prevent accidentally cheating",
+			extra = "Affects command list: /gamemode, /tp, /teleport, /give, /setblock, /summon",
+			category = {TIS, SURVIVAL}
+	)
+	public static boolean opPlayerNoCheat = false;
+
 	/*
 	 *   Declare rules above this
 	 *   General validators down below
---
+++ src/main/java/carpettisaddition/helpers/CommandPermissionHelper.java
@@ -0,0 +1,14 @@
+package carpettisaddition.helpers;
+
+import carpettisaddition.CarpetTISAdditionSettings;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+
+public class CommandPermissionHelper
+{
+	public static boolean canCheat(ServerCommandSource source, int level)
+	{
+		return source.hasPermissionLevel(level) && !((source.getEntity() instanceof ServerPlayerEntity) && CarpetTISAdditionSettings.opPlayerNoCheat);
+	}
+}
