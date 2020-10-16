f478f498f56f0981ef4d6c810f121f1046186bbb
Fallen_Breath <yqx791125833@gmail.com>
2020-10-16 14:59:25 +0800

    MicroTick: attach logger to ServerWorld now

--- src/main/java/carpettisaddition/mixins/logger/microtick/ServerWorldMixin.java
+++ src/main/java/carpettisaddition/mixins/logger/microtick/ServerWorldMixin.java
@@ -2,8 +2,13 @@ package carpettisaddition.mixins.logger.microtick;
 
 import carpettisaddition.interfaces.IServerWorld_MicroTickLogger;
 import carpettisaddition.logging.loggers.microtick.MicroTickLogger;
+import carpettisaddition.logging.loggers.microtick.MicroTickLoggerManager;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.level.LevelProperties;
+import net.minecraft.world.level.ServerWorldProperties;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,5 +31,21 @@ public abstract class ServerWorldMixin implements IServerWorld_MicroTickLogger
 	public MicroTickLogger getMicroTickLogger()
 	{
 		return this.microTickLogger;
+	}
+
+	@Shadow
+	@Final
+	private ServerWorldProperties worldProperties;
+
+	@Inject(
+			method = "tickTime",
+			at = @At(value = "HEAD")
+	)
+	private void onTimeUpdate(CallbackInfo ci)
+	{
+		if (this.worldProperties.getClass() == LevelProperties.class)  // only flush messages main world time update
+		{
+			MicroTickLoggerManager.flushMessages();
+		}
 	}
 }
