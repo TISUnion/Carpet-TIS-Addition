4a36d4ecf61f6b64193a9b0cb6434e5cf88111b1
Fallen_Breath <yqx791125833@gmail.com>
2020-10-16 14:51:30 +0800

    MicroTick: attach logger to ServerWorld now

--- src/main/java/carpettisaddition/interfaces/IServerWorld_MicroTickLogger.java
+++ src/main/java/carpettisaddition/interfaces/IServerWorld_MicroTickLogger.java
@@ -2,7 +2,7 @@ package carpettisaddition.interfaces;
 
 import carpettisaddition.logging.loggers.microtick.MicroTickLogger;
 
-public interface IWorld_MicroTickLogger
+public interface IServerWorld_MicroTickLogger
 {
 	MicroTickLogger getMicroTickLogger();
 }
