66e0c2604490599d89a3033ed68aec6dfa4eed9d
Fallen_Breath <yqx791125833@gmail.com>
2020-11-09 18:36:19 +0800

    fix crash when mob spawns

--- src/main/java/carpettisaddition/mixins/logger/microtiming/tickstages/SpawnHelperMixin.java
+++ src/main/java/carpettisaddition/mixins/logger/microtiming/tickstages/SpawnHelperMixin.java
@@ -3,9 +3,9 @@ package carpettisaddition.mixins.logger.microtiming.tickstages;
 import carpettisaddition.logging.loggers.microtiming.MicroTimingLoggerManager;
 import carpettisaddition.logging.loggers.microtiming.enums.TickStage;
 import net.minecraft.entity.EntityCategory;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,8 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class SpawnHelperMixin
 {
 	@Inject(method = "spawnEntitiesInChunk", at = @At("HEAD"))
-	private static void onStageSpawn(EntityCategory category, ServerWorld serverWorld, WorldChunk chunk, BlockPos spawnPos, CallbackInfo ci)
+	private static void onStageSpawn(EntityCategory category, World world, WorldChunk chunk, BlockPos spawnPos, CallbackInfo ci)
 	{
-		MicroTimingLoggerManager.setTickStage(serverWorld, TickStage.SPAWNING);
+		MicroTimingLoggerManager.setTickStage(world, TickStage.SPAWNING);
 	}
 }
