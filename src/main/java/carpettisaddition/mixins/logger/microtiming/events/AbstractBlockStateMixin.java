4b7b7af864e8ab70d1a5fd68e8d8d3a059ad8cf7
Fallen_Breath <yqx791125833@gmail.com>
2020-11-12 11:33:38 +0800

    fix: microTiming logger: state update not detectable

--- src/main/java/carpettisaddition/mixins/logger/microtiming/events/AbstractBlockStateMixin.java
+++ src/main/java/carpettisaddition/mixins/logger/microtiming/events/AbstractBlockStateMixin.java
@@ -3,21 +3,20 @@ package carpettisaddition.mixins.logger.microtiming.events;
 import carpettisaddition.logging.loggers.microtiming.MicroTimingLoggerManager;
 import carpettisaddition.logging.loggers.microtiming.enums.BlockUpdateType;
 import carpettisaddition.logging.loggers.microtiming.enums.EventType;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Block.class)
-public abstract class BlockMixin
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public abstract class AbstractBlockStateMixin
 {
-	@Inject(method = "postProcessState", at = @At("HEAD"))
-	private static void startStateUpdate(BlockState state, WorldAccess worldAccess, BlockPos pos, CallbackInfoReturnable<BlockState> cir)
+	@Inject(method = "updateNeighbors(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;II)V", at = @At("HEAD"))
+	private void startStateUpdate(WorldAccess worldAccess, BlockPos pos, int flags, int maxUpdateDepth, CallbackInfo ci)
 	{
 		if (worldAccess instanceof World)
 		{
@@ -25,8 +24,8 @@ public abstract class BlockMixin
 		}
 	}
 
-	@Inject(method = "postProcessState", at = @At("RETURN"))
-	private static void endStateUpdate(BlockState state, WorldAccess worldAccess, BlockPos pos, CallbackInfoReturnable<BlockState> cir)
+	@Inject(method = "updateNeighbors(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;II)V", at = @At("RETURN"))
+	private void endStateUpdate(WorldAccess worldAccess, BlockPos pos, int flags, int maxUpdateDepth, CallbackInfo ci)
 	{
 		if (worldAccess instanceof World)
 		{
