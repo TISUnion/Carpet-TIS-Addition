5d9f5c34be256187a9093516d229ae3f731590fc
Fallen_Breath <yqx791125833@gmail.com>
2020-11-26 12:05:45 +0800

    repeaterHalfDelay working

--- src/main/java/carpettisaddition/mixins/rule/repeaterHalfDelay/AbstractRedstoneGateBlockMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/repeaterHalfDelay/AbstractRedstoneGateBlockMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.AbstractRedstoneGateBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.RepeaterBlock;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,7 +26,7 @@ public abstract class AbstractRedstoneGateBlockMixin
 					target = "Lnet/minecraft/block/AbstractRedstoneGateBlock;getUpdateDelayInternal(Lnet/minecraft/block/BlockState;)I"
 			)
 	)
-	private int modifyRepeaterDelay(AbstractRedstoneGateBlock abstractRedstoneGateBlock, BlockState state1, BlockState state2, ServerWorld world, BlockPos pos, Random random)
+	private int modifyRepeaterDelay(AbstractRedstoneGateBlock abstractRedstoneGateBlock, BlockState state1, BlockState state2, World world, BlockPos pos, Random random)
 	{
 		return this.getModifiedDelay(abstractRedstoneGateBlock, world, pos, state1);
 	}
