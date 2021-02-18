979413fd5f27f32737e28b91a19db50469957fb9
Fallen_Breath <yqx791125833@gmail.com>
2021-02-18 09:00:40 +0800

    rule fluidDestructionDisabled fix

--- src/main/java/carpettisaddition/mixins/rule/fluidDestructionDisabled/FlowableFluidMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/fluidDestructionDisabled/FlowableFluidMixin.java
@@ -1,20 +1,20 @@
 package carpettisaddition.mixins.rule.fluidDestructionDisabled;
 
 import carpettisaddition.CarpetTISAdditionSettings;
-import net.minecraft.fluid.BaseFluid;
+import net.minecraft.fluid.FlowableFluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BaseFluid.class)
-public class BaseFluidMixin
+@Mixin(FlowableFluid.class)
+public class FlowableFluidMixin
 {
 	@Inject(
 			method = "flow",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/fluid/BaseFluid;beforeBreakingBlock(Lnet/minecraft/world/IWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V"
+					target = "Lnet/minecraft/fluid/FlowableFluid;beforeBreakingBlock(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V"
 			),
 			cancellable = true
 	)
