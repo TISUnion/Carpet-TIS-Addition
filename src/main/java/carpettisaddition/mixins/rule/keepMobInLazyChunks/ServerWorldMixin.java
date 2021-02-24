7fd61b9e9c1f6cf8ba06bf2d469270b713bad23a
Fallen_Breath <yqx791125833@gmail.com>
2021-02-24 23:46:06 +0800

    update ServerWorldMixin for rule keepMobInLazyChunks to make mixin happy

--- src/main/java/carpettisaddition/mixins/rule/keepMobInLazyChunks/ServerWorldMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/keepMobInLazyChunks/ServerWorldMixin.java
@@ -3,22 +3,27 @@ package carpettisaddition.mixins.rule.keepMobInLazyChunks;
 import carpettisaddition.CarpetTISAdditionSettings;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin
 {
-    // lambda method in method ServerWorld#tick
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Redirect(method = "method_31420", at = @At(
-        value = "INVOKE",
-        target = "Lnet/minecraft/entity/Entity;checkDespawn()V"
-    ))
+    @Dynamic
+    @SuppressWarnings("DefaultAnnotationParam")
+    @Redirect(
+            method = "method_31420",  // lambda method in method ServerWorld#tick
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;checkDespawn()V",
+                    remap = true
+            ),
+            remap = false
+    )
     private void dummyCheckDespawn(Entity entity) {
-        if(!CarpetTISAdditionSettings.keepMobInLazyChunks)
+        if (!CarpetTISAdditionSettings.keepMobInLazyChunks)
             entity.checkDespawn();
     }
 }
