dfee46f2e6092ee0b7f2160a04956a9f149d6912
Fallen_Breath <yqx791125833@gmail.com>
2021-03-01 10:44:24 +0800

    turtle egg logger?

---
+++ src/main/java/carpettisaddition/logging/loggers/turtleegg/TurtleEggLogger.java
@@ -0,0 +1,48 @@
+package carpettisaddition.logging.loggers.turtleegg;
+
+import carpet.logging.LoggerRegistry;
+import carpet.utils.Messenger;
+import carpettisaddition.logging.ExtensionLoggerRegistry;
+import carpettisaddition.logging.loggers.AbstractLogger;
+import carpettisaddition.utils.TextUtil;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.BaseText;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TurtleEggLogger extends AbstractLogger
+{
+	public static final String NAME = "turtleEgg";
+
+	private static final TurtleEggLogger INSTANCE = new TurtleEggLogger();
+
+	private TurtleEggLogger()
+	{
+		super(NAME);
+	}
+
+	public static TurtleEggLogger getInstance()
+	{
+		return INSTANCE;
+	}
+
+	public void onEggBroken(World world, BlockPos pos, BlockState state, Entity entity)
+	{
+		if (!world.isClient && ExtensionLoggerRegistry.__turtleEgg)
+		{
+			return;
+		}
+		LoggerRegistry.getLogger(NAME).log(() -> {
+			//  xxx breaks egg @ {}
+			return new BaseText[]{Messenger.c(
+					"g [\uD83E\uDD5A] ",
+					entity.getName(),
+					"w  x ",
+					TextUtil.getBlockName(state.getBlock()),
+					"g  @ ",
+					TextUtil.getCoordinateText(null, pos, world.getDimension().getType())
+			)};
+		});
+	}
+}
---
+++ src/main/java/carpettisaddition/mixins/logger/turtleegg/TurtleEggBlockMixin.java
@@ -0,0 +1,39 @@
+package carpettisaddition.mixins.logger.turtleegg;
+
+import carpettisaddition.logging.loggers.turtleegg.TurtleEggLogger;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TurtleEggBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TurtleEggBlock.class)
+public abstract class TurtleEggBlockMixin
+{
+	private Entity breakingEggEntity;
+
+	@Inject(method = "breakEgg", at = @At("TAIL"))
+	private void onEggBrokenTurtleEggLogger(World world, BlockPos pos, BlockState state, CallbackInfo ci)
+	{
+		TurtleEggLogger.getInstance().onEggBroken(world, pos, state, this.breakingEggEntity);
+	}
+
+	@Inject(method = "tryBreakEgg", at = @At("HEAD"))
+	private void recordEntityTurtleEggLogger(World world, BlockPos pos, Entity entity, int inverseChance, CallbackInfo ci)
+	{
+		this.breakingEggEntity = entity;
+	}
+
+	@Inject(method = "afterBreak", at = @At("HEAD"))
+	private void recordEntityTurtleEggLogger(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack stack, CallbackInfo ci)
+	{
+		this.breakingEggEntity = player;
+	}
+}
