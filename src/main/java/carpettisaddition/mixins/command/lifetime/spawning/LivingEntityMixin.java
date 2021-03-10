8aeaf8ce4abeee4f1c96ecc8f9816f62ddd24b69
Fallen_Breath <yqx791125833@gmail.com>
2021-03-10 17:37:41 +0800

    replaced some @Inject with LocalCapture with @ModifyArg

--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LivingEntityMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LivingEntityMixin.java
@@ -4,15 +4,11 @@ import carpettisaddition.commands.lifetime.interfaces.IEntity;
 import carpettisaddition.commands.lifetime.spawning.MobDropSpawningReason;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity
@@ -23,17 +19,17 @@ public abstract class LivingEntityMixin extends Entity
 	}
 
 	// wither rose thing
-	@Inject(
+	@ModifyArg(
 			method = "onKilledBy",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
-			),
-			locals = LocalCapture.CAPTURE_FAILHARD
+			)
 	)
-	private void onMobDroppedItemLifeTimeTracker(LivingEntity adversary, CallbackInfo ci, boolean bl, ItemEntity itemEntity)
+	private Entity onMobDroppedItemLifeTimeTracker(Entity itemEntity)
 	{
 		((IEntity)itemEntity).recordSpawning(new MobDropSpawningReason(this.getType()));
+		return itemEntity;
 	}
 
 	@ModifyArg(
--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/SlimeEntityMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/SlimeEntityMixin.java
@@ -2,29 +2,25 @@ package carpettisaddition.mixins.command.lifetime.spawning;
 
 import carpettisaddition.commands.lifetime.interfaces.IEntity;
 import carpettisaddition.commands.lifetime.spawning.LiteralSpawningReason;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.SlimeEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(SlimeEntity.class)
 public abstract class SlimeEntityMixin
 {
-	@Inject(
+	@ModifyArg(
 			method = "remove",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
-			),
-			locals = LocalCapture.CAPTURE_FAILHARD
+			)
 	)
-	private void onSlimeDivisionLifeTimeTracker(CallbackInfo ci, int i, int j, int k, float f, float g, SlimeEntity slimeEntity)
+	private Entity onSlimeDivisionLifeTimeTracker(Entity slimeEntity)
 	{
-		if (slimeEntity != null)
-		{
-			((IEntity)slimeEntity).recordSpawning(LiteralSpawningReason.SLIME);
-		}
+		((IEntity)slimeEntity).recordSpawning(LiteralSpawningReason.SLIME);
+		return slimeEntity;
 	}
 }
