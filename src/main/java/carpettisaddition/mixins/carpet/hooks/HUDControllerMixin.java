51a2981a80f391d59d48d520779aec123772b2ce
Fallen_Breath <yqx791125833@gmail.com>
2021-04-19 16:13:29 +0800

    carpet compatibility thing: HUDController hook

--- src/main/java/carpettisaddition/mixins/carpet/hooks/HUDControllerMixin.java
+++ src/main/java/carpettisaddition/mixins/carpet/hooks/HUDControllerMixin.java
@@ -4,24 +4,24 @@ import carpet.logging.HUDController;
 import carpettisaddition.logging.TISAdditionHUDController;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.List;
+import java.util.function.Consumer;
 
+/**
+ * Using mixin instead of directly invoking HUDController#register should have better carpet version compatibility,
+ * since HUDController#register is not a static method, which doesn't seem to be intentional and might gets changed
+ * in the future version
+ */
 @Mixin(HUDController.class)
 public abstract class HUDControllerMixin
 {
-	@Inject(
-			method = "update_hud",
-			at = @At(
-					value = "INVOKE",
-					target = "Ljava/util/Map;keySet()Ljava/util/Set;"
-			),
-			remap = false
-	)
-	private static void updateTISAdditionHUDLoggers(MinecraftServer server, CallbackInfo ci)
+	@Shadow(remap = false)
+	private static List<Consumer<MinecraftServer>> HUDListeners;
+
+	static
 	{
-		TISAdditionHUDController.updateHUD(server);
+		HUDListeners.add(TISAdditionHUDController::updateHUD);
 	}
 }
