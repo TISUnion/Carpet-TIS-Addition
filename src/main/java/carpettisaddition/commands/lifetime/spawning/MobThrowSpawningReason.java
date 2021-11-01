50bfa5c509326e66886fcec577b96dcea70cde97
Fallen_Breath <yqx791125833@gmail.com>
2021-11-02 01:03:22 +0800

    lifetime tracker: mob related spawning/removal reason formatting

--- src/main/java/carpettisaddition/commands/lifetime/spawning/MobThrowSpawningReason.java
+++ src/main/java/carpettisaddition/commands/lifetime/spawning/MobThrowSpawningReason.java
@@ -1,38 +1,18 @@
 package carpettisaddition.commands.lifetime.spawning;
 
-import carpet.utils.Messenger;
 import net.minecraft.entity.EntityType;
 import net.minecraft.text.BaseText;
 
-import java.util.Objects;
-
-// for item or other entity intentionally throw by entity
-public class MobThrowSpawningReason extends SpawningReason {
-	private final EntityType<?> providerType;
-
-	public MobThrowSpawningReason(EntityType<?> providerType) {
-		this.providerType = providerType;
+public class MobThrowSpawningReason extends MobRelatedSpawningReason
+{
+	public MobThrowSpawningReason(EntityType<?> providerType)
+	{
+		super(providerType);
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		MobThrowSpawningReason that = (MobThrowSpawningReason) o;
-		return Objects.equals(providerType, that.providerType);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(providerType);
-	}
-
-	@Override
-	public BaseText toText() {
-		return Messenger.c(
-				"w " + this.tr("mob_throw.pre", "Thrown by "),
-				this.providerType.getName(),
-				"w " + this.tr("mob_throw.post", "")
-		);
+	public BaseText toText()
+	{
+		return this.advTr("mob_throw", "Thrown by %1$s", this.providerType.getName());
 	}
 }
