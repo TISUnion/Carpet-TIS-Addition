3c87842e3f1eadc286cadf64dce71a812bc1873b
Fallen_Breath <yqx791125833@gmail.com>
2021-11-05 00:03:25 +0800

    neater Messenger.formatting with parseCarpetStyle

--- src/main/java/carpettisaddition/mixins/translations/StyleAccessor.java
+++ src/main/java/carpettisaddition/mixins/translations/StyleAccessor.java
@@ -2,12 +2,31 @@ package carpettisaddition.mixins.translations;
 
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Style.class)
 public interface StyleAccessor
 {
+	@Accessor("bold")
+	Boolean getBoldField();
+
+	@Accessor("italic")
+	Boolean getItalicField();
+
+	@Accessor("underline")
+	Boolean getUnderlineField();
+
+	@Accessor("strikethrough")
+	Boolean getStrikethroughField();
+
+	@Accessor("obfuscated")
+	Boolean getObfuscatedField();
+
+	@Accessor("color")
+	Formatting getColorField();
+
 	@Accessor("hoverEvent")
 	HoverEvent getHoverEventField();
 }
