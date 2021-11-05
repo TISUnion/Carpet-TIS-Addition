30e1503e0a0e5ea500a9669c71d2318219394c54
Fallen_Breath <yqx791125833@gmail.com>
2021-11-05 13:45:56 +0800

    no crash here

--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LookTargetUtilMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LookTargetUtilMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.task.LookTargetUtil;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,7 +24,7 @@ public class LookTargetUtilMixin {
                     target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
             )
     )
-    private static void onThrowItemEntityLifeTimeTracker(LivingEntity entity, ItemStack stack, LivingEntity target, CallbackInfo ci, double ignored, ItemEntity itemEntity) {
+    private static void onThrowItemEntityLifeTimeTracker(LivingEntity entity, ItemStack stack, Vec3d targetLocation, CallbackInfo ci, double ignored, ItemEntity itemEntity) {
         ((LifetimeTrackerTarget) itemEntity).recordSpawning(new MobThrowSpawningReason(entity.getType()));
     }
 }
