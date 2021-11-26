facfa31d57ed20e8b84352b422d3dc9f96253f29
Fallen_Breath <yqx791125833@gmail.com>
2021-11-27 04:33:44 +0800

    rule `voidRelatedAltitude` porting to 17

--- src/main/java/carpettisaddition/mixins/rule/voidRelatedAltitude/EntityMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/voidRelatedAltitude/EntityMixin.java
@@ -2,6 +2,7 @@ package carpettisaddition.mixins.rule.voidRelatedAltitude;
 
 import carpettisaddition.CarpetTISAdditionSettings;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -9,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(Entity.class)
 public abstract class EntityMixin
 {
-	@ModifyConstant(method = "baseTick", constant = @Constant(doubleValue = -64.0D), allow = 1, require = 0)
-	private double modifyVoidRelatedAltitude(double value)
+	@ModifyConstant(method = "attemptTickInVoid", constant = @Constant(intValue = 64), allow = 1, require = 0)
+	private int modifyVoidRelatedAltitude(int value)
 	{
-		return CarpetTISAdditionSettings.voidRelatedAltitude;
+		return (int)MathHelper.clamp(Math.round(-CarpetTISAdditionSettings.voidRelatedAltitude), 0, Integer.MAX_VALUE);
 	}
 }
