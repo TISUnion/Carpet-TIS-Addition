c5c53bfd80708fb4283897dfd415d8a2fbfb3ca9
Fallen_Breath <yqx791125833@gmail.com>
2021-12-27 18:41:31 +0800

    merging to 16

--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/breeding/AnimalEntityMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/breeding/AnimalEntityMixin.java
@@ -3,19 +3,19 @@ package carpettisaddition.mixins.command.lifetime.spawning.breeding;
 import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
 import carpettisaddition.commands.lifetime.spawning.LiteralSpawningReason;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.goal.AnimalMateGoal;
+import net.minecraft.entity.passive.AnimalEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(AnimalMateGoal.class)
-public abstract class AnimalMateGoalMixin
+@Mixin(AnimalEntity.class)
+public abstract class AnimalEntityMixin
 {
 	@ModifyArg(
 			method = "breed",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z",
+					target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V",
 					ordinal = 0
 			)
 	)
--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/breeding/TurtleEggBlockMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/breeding/TurtleEggBlockMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class TurtleEggBlockMixin
 {
 	@ModifyArg(
-			method = "scheduledTick",
+			method = "randomTick",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/server/world/ServerWorld;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
