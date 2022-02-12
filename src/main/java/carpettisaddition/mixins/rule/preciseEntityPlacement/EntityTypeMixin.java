1fcc9df628f73964a5c23214d059e869b704045d
Fallen_Breath <yqx791125833@gmail.com>
2022-02-13 00:09:02 +0800

    neater

--- src/main/java/carpettisaddition/mixins/rule/preciseEntityPlacement/EntityTypeMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/preciseEntityPlacement/EntityTypeMixin.java
@@ -13,30 +13,11 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityType.class)
 public abstract class EntityTypeMixin<T extends Entity>
 {
-	private boolean shouldApplyPreciseEntityPlacement = false;
-
-	@Inject(method = "spawnFromItemStack", at = @At("HEAD"))
-	private void preciseEntityPlacementSetFlag(CallbackInfoReturnable<Entity> cir)
-	{
-		if (CarpetTISAdditionSettings.preciseEntityPlacement)
-		{
-			this.shouldApplyPreciseEntityPlacement = true;
-		}
-	}
-
-	@Inject(method = "spawnFromItemStack", at = @At("TAIL"))
-	private void preciseEntityPlacementRemoveFlag(CallbackInfoReturnable<Entity> cir)
-	{
-		this.shouldApplyPreciseEntityPlacement = false;
-	}
-
 	@ModifyVariable(
 			method = "create(Lnet/minecraft/world/World;Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/text/Text;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/SpawnType;ZZ)Lnet/minecraft/entity/Entity;",
 			at = @At(
@@ -47,14 +28,12 @@ public abstract class EntityTypeMixin<T extends Entity>
 	)
 	private T preciseEntityPlacement(T entity, World world, @Nullable CompoundTag itemTag, @Nullable Text name, @Nullable PlayerEntity player, BlockPos pos, SpawnType spawnType, boolean alignPosition, boolean invertY)
 	{
-		// in case of multi-threading, there's an extra spawnEggTargetPos null check in
-		// the method PreciseEntityPlacer#adjustEntityFromSpawnEgg,
-		// so using non-thread-local flag field (shouldApplyPreciseEntityPlacement) is fine
-		if (CarpetTISAdditionSettings.preciseEntityPlacement && this.shouldApplyPreciseEntityPlacement)
+		// there's an extra spawnEggTargetPos null check in the method PreciseEntityPlacer#adjustEntityFromSpawnEgg
+		// testing if the adjusting is required
+		if (CarpetTISAdditionSettings.preciseEntityPlacement)
 		{
 			PreciseEntityPlacer.adjustEntityFromSpawnEgg(entity);
 		}
-
 		return entity;
 	}
 }
