8c3dc61e10cf9a26351637165b41c47a33a3fda9
Fallen_Breath <yqx791125833@gmail.com>
2022-03-14 12:55:08 +0800

    mapping update

--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LargeEntitySpawnHelperMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/LargeEntitySpawnHelperMixin.java
@@ -2,8 +2,8 @@ package carpettisaddition.mixins.command.lifetime.spawning;
 
 import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
 import carpettisaddition.commands.lifetime.spawning.LiteralSpawningReason;
-import net.minecraft.class_6959;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.LargeEntitySpawnHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
-// Summon Util or whatever
-@Mixin(class_6959.class)
-public abstract class class_6959Mixin
+@Mixin(LargeEntitySpawnHelper.class)
+public abstract class LargeEntitySpawnHelperMixin
 {
-	@Inject(method = "method_40585", at= @At("RETURN"))
+	@Inject(method = "trySpawnAt", at= @At("RETURN"))
 	private static <T extends MobEntity> void onEntitySummonLifeTimeTracker(CallbackInfoReturnable<Optional<T>> cir)
 	{
 		cir.getReturnValue().ifPresent(entity -> ((LifetimeTrackerTarget)entity).recordSpawning(LiteralSpawningReason.SUMMON));
