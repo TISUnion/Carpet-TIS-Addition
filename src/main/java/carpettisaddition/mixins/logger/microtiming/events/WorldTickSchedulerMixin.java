7b8dda73ddec51f614a27e4e7e5f67a9591926c4
Fallen_Breath <yqx791125833@gmail.com>
2022-03-25 16:23:23 +0800

    microtiming: added back ExecuteTileTickEvent

--- src/main/java/carpettisaddition/mixins/logger/microtiming/events/WorldTickSchedulerMixin.java
+++ src/main/java/carpettisaddition/mixins/logger/microtiming/events/WorldTickSchedulerMixin.java
@@ -1,6 +1,7 @@
 package carpettisaddition.mixins.logger.microtiming.events;
 
 import carpettisaddition.logging.loggers.microtiming.MicroTimingLoggerManager;
+import carpettisaddition.logging.loggers.microtiming.enums.EventType;
 import carpettisaddition.logging.loggers.microtiming.interfaces.ITileTickListWithServerWorld;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.tick.ChunkTickScheduler;
@@ -9,6 +10,7 @@ import net.minecraft.world.tick.WorldTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -47,5 +49,42 @@ public abstract class WorldTickSchedulerMixin<T>
 			int delay = (int)(tt.triggerTick() - serverWorld.getTime());
 			MicroTimingLoggerManager.onScheduleTileTickEvent(serverWorld, tt.type(), tt.pos(), delay, tt.priority(), chunkTickScheduler.getTickCount() > this.oldListSize);
 		}
+	}
+
+	@ModifyVariable(
+			method = "tick(Ljava/util/function/BiConsumer;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/function/BiConsumer;accept(Ljava/lang/Object;Ljava/lang/Object;)V",
+					remap = false
+			)
+	)
+	private OrderedTick<T> preExecuteBlockTileTickEvent(OrderedTick<T> event)
+	{
+		ServerWorld serverWorld = ((ITileTickListWithServerWorld)this).getServerWorld();
+		if (serverWorld != null)
+		{
+			MicroTimingLoggerManager.onExecuteTileTickEvent(serverWorld, event, EventType.ACTION_START);
+		}
+		return event;
+	}
+
+	@ModifyVariable(
+			method = "tick(Ljava/util/function/BiConsumer;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/function/BiConsumer;accept(Ljava/lang/Object;Ljava/lang/Object;)V",
+					shift = At.Shift.AFTER,
+					remap = false
+			)
+	)
+	private OrderedTick<T> postExecuteBlockTileTickEvent(OrderedTick<T> event)
+	{
+		ServerWorld serverWorld = ((ITileTickListWithServerWorld)this).getServerWorld();
+		if (serverWorld != null)
+		{
+			MicroTimingLoggerManager.onExecuteTileTickEvent(serverWorld, event, EventType.ACTION_START);
+		}
+		return event;
 	}
 }
