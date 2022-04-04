9ba71624b6588482a3a12b7414b3fc66f842e6a8
Fallen_Breath <yqx791125833@gmail.com>
2022-04-04 16:48:28 +0800

    lifetime tracker: records dolphin pickup / throw item

---
+++ src/main/java/carpettisaddition/mixins/command/lifetime/removal/DolphinEntityMixin.java
@@ -0,0 +1,34 @@
+package carpettisaddition.mixins.command.lifetime.removal;
+
+import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
+import carpettisaddition.commands.lifetime.removal.MobPickupRemovalReason;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.passive.DolphinEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DolphinEntity.class)
+public abstract class DolphinEntityMixin extends Entity
+{
+	public DolphinEntityMixin(EntityType<?> type, World world)
+	{
+		super(type, world);
+	}
+
+	@Inject(
+			method = "loot",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/entity/passive/DolphinEntity;sendPickup(Lnet/minecraft/entity/Entity;I)V"
+			)
+	)
+	private void onEntityPersistent2LifeTimeTracker(ItemEntity item, CallbackInfo ci)
+	{
+		((LifetimeTrackerTarget)item).recordRemoval(new MobPickupRemovalReason(this.getType()));
+	}
+}
---
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/mobthrow/DolphinEntityPlayWithItemsGoalMixin.java
@@ -0,0 +1,31 @@
+package carpettisaddition.mixins.command.lifetime.spawning.mobthrow;
+
+import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
+import carpettisaddition.commands.lifetime.spawning.MobThrowSpawningReason;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.passive.DolphinEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(targets = "net.minecraft.entity.passive.DolphinEntity$PlayWithItemsGoal")
+public abstract class DolphinEntityPlayWithItemsGoalMixin
+{
+	@Shadow(remap = false) @Final
+	DolphinEntity field_6757;
+
+	@ModifyVariable(
+			method = "spitOutItem",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
+			)
+	)
+	private ItemEntity onDolphinThrowItemEntityLifeTimeTracker(ItemEntity itemEntity)
+	{
+		((LifetimeTrackerTarget)itemEntity).recordSpawning(new MobThrowSpawningReason(this.field_6757.getType()));
+		return itemEntity;
+	}
+}
