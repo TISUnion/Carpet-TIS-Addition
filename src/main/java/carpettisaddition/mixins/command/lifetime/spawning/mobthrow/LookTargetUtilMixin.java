523a6bf23ae4696db2d9b08399ce26e5d3b49c49
Fallen_Breath <yqx791125833@gmail.com>
2022-04-04 16:32:03 +0800

    lifetime tracker: fixed wrong thrower type in some MobThrowSpawningReason

--- src/main/java/carpettisaddition/mixins/command/lifetime/spawning/mobthrow/LookTargetUtilMixin.java
+++ src/main/java/carpettisaddition/mixins/command/lifetime/spawning/mobthrow/LookTargetUtilMixin.java
@@ -3,7 +3,9 @@ package carpettisaddition.mixins.command.lifetime.spawning.mobthrow;
 import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
 import carpettisaddition.commands.lifetime.spawning.MobThrowSpawningReason;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.task.LookTargetUtil;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -18,9 +20,9 @@ public abstract class LookTargetUtilMixin
 					target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
 			)
 	)
-	private static ItemEntity onThrowItemEntityLifeTimeTracker(ItemEntity itemEntity)
+	private static ItemEntity onThrowItemEntityLifeTimeTracker(ItemEntity itemEntity, LivingEntity entity, ItemStack stack, LivingEntity target)
 	{
-		((LifetimeTrackerTarget) itemEntity).recordSpawning(new MobThrowSpawningReason(itemEntity.getType()));
+		((LifetimeTrackerTarget) itemEntity).recordSpawning(new MobThrowSpawningReason(entity.getType()));
 		return itemEntity;
 	}
 }
