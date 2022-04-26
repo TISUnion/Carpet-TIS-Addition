8f650461928328dd56fcf02fd3fb1be55665ac0e
Fallen_Breath <yqx791125833@gmail.com>
2022-04-27 04:04:30 +0800

    it's not that easy to create a tile entity ignoring the target block tho
    then remove subcommand is not necessary too

--- src/main/java/carpettisaddition/commands/manipulate/container/TileEntityListController.java
+++ src/main/java/carpettisaddition/commands/manipulate/container/TileEntityListController.java
@@ -7,34 +7,19 @@ import carpettisaddition.utils.compat.DimensionWrapper;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 import com.mojang.brigadier.builder.ArgumentBuilder;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.BaseText;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static net.minecraft.command.arguments.BlockPosArgumentType.blockPos;
 import static net.minecraft.command.arguments.BlockPosArgumentType.getLoadedBlockPos;
-import static net.minecraft.command.arguments.IdentifierArgumentType.getIdentifier;
-import static net.minecraft.command.arguments.IdentifierArgumentType.identifier;
-import static net.minecraft.command.arguments.NbtCompoundTagArgumentType.getCompoundTag;
-import static net.minecraft.command.arguments.NbtCompoundTagArgumentType.nbtCompound;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -81,55 +66,6 @@ public class TileEntityListController extends AbstractEntityListController
 		}
 	}
 
-	private int removeAt(ServerCommandSource source, BlockPos pos)
-	{
-		ServerWorld world = source.getWorld();
-		world.removeBlockEntity(pos);
-		Messenger.tell(source, tr("remove", Messenger.coord(pos, DimensionWrapper.of(world))));
-		return 1;
-	}
-
-	private int createAt(CommandContext<ServerCommandSource> context) throws CommandSyntaxException
-	{
-		ServerCommandSource source = context.getSource();
-		BlockPos blockPos = getLoadedBlockPos(context, "pos");
-		Identifier id = getIdentifier(context, "block");
-
-		Block block = Registry.BLOCK.get(id);
-		if (!(block instanceof BlockEntityProvider))
-		{
-			Messenger.tell(source, tr("create.not_provider", id));
-			return 0;
-		}
-
-		CompoundTag nbt = null;
-		try
-		{
-			nbt = getCompoundTag(context, "data");
-		}
-		catch (IllegalArgumentException ignored)
-		{
-		}
-
-		ServerWorld world = source.getWorld();
-		BlockEntity blockEntity = ((BlockEntityProvider) block).createBlockEntity(world);
-		if (blockEntity != null)
-		{
-			if (nbt != null)
-			{
-				blockEntity.fromTag(nbt);
-			}
-			world.setBlockEntity(blockPos, blockEntity);
-			Messenger.tell(source, tr("create.created", Messenger.blockEntity(blockEntity)));
-			return 1;
-		}
-		else
-		{
-			Messenger.tell(source,  Messenger.formatting(tr("create.failed"), "r"));
-			return 0;
-		}
-	}
-
 	private void showTopNInCollection(ServerCommandSource source, BaseText name, Collection<BlockEntity> blockEntities)
 	{
 		Multiset<BlockEntityType<?>> counter = HashMultiset.create();
@@ -139,24 +75,27 @@ public class TileEntityListController extends AbstractEntityListController
 				limit(TOP_N).
 				collect(Collectors.toList());
 
-		Messenger.tell(source, Messenger.format("- %1$s: %2$sx", name, blockEntities.size()));
+		Messenger.tell(source, Messenger.format("%1$s: %2$sx", name, blockEntities.size()));
 		for (int i = 0; i < topN.size(); i++)
 		{
 			Multiset.Entry<BlockEntityType<?>> entry = topN.get(i);
-			Messenger.tell(source, Messenger.format(
-					"  %1$s. %2$s: %3$sx",
+			Messenger.tell(source, Messenger.formatting(Messenger.format(
+					"%1$s. %2$s: %3$sx",
 					i + 1,
 					IdentifierUtil.id(entry.getElement()),
 					entry.getCount()
-			));
+			), "g"));
 		}
 	}
 
 	private int showStatistic(ServerCommandSource source)
 	{
 		ServerWorld world = source.getWorld();
-		Messenger.tell(source, tr("statistic.title", Messenger.dimension(DimensionWrapper.of(world))));
-		this.showTopNInCollection(source, tr("statistic.total"), world.blockEntities);
+		Messenger.tell(source, Messenger.format(
+				"===== %1$s =====",
+				tr("statistic.title", Messenger.dimension(DimensionWrapper.of(world)))
+		));
+		this.showTopNInCollection(source, tr("statistic.all"), world.blockEntities);
 		this.showTopNInCollection(source, tr("statistic.ticking"), world.tickingBlockEntities);
 		return 1;
 	}
@@ -172,32 +111,6 @@ public class TileEntityListController extends AbstractEntityListController
 						then(argument("pos", blockPos()).
 								executes(c -> this.queryTileEntityInfo(c.getSource(), getLoadedBlockPos(c, "pos")))
 						)
-				).
-				then(literal("remove").
-						then(argument("pos", blockPos()).
-								executes(c -> this.removeAt(c.getSource(), getLoadedBlockPos(c, "pos")))
-						)
-				).
-				then(literal("create").
-						then(argument("pos", blockPos()).
-								then(argument("block", identifier()).
-										suggests(this::suggestBlockWithEntity).
-										executes(this::createAt).
-										then(argument("data", nbtCompound()).
-												executes(this::createAt)
-										)
-								)
-						)
 				);
-	}
-
-	private CompletableFuture<Suggestions> suggestBlockWithEntity(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder)
-	{
-		return CommandSource.suggestIdentifiers(
-				Registry.BLOCK.stream().
-						filter(block -> block instanceof BlockEntityProvider).
-						map(Registry.BLOCK::getId),
-				builder
-		);
 	}
 }
