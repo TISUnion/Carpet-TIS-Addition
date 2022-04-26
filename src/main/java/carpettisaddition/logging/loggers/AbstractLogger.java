74400eee23647f6956f482fefca948c8bdf2abba
Fallen_Breath <yqx791125833@gmail.com>
2022-04-26 20:42:42 +0800

    nicer logger logging utils

--- src/main/java/carpettisaddition/logging/loggers/AbstractLogger.java
+++ src/main/java/carpettisaddition/logging/loggers/AbstractLogger.java
@@ -1,13 +1,72 @@
 package carpettisaddition.logging.loggers;
 
+import carpet.logging.Logger;
+import carpet.logging.LoggerRegistry;
+import carpettisaddition.CarpetTISAdditionServer;
 import carpettisaddition.translations.TranslationContext;
+import com.google.common.base.Joiner;
+import net.minecraft.text.BaseText;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public abstract class AbstractLogger extends TranslationContext
 {
-	protected final static String MULTI_OPTION_SEP_REG = "[,. ]";
+	public final static String MULTI_OPTION_SEP_REG = "[,. ]";
+	public final static String OPTION_SEP = ".";
+
+	private final String name;
 
 	public AbstractLogger(String name)
 	{
 		super("logger." + name);
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	// Carpet Logging
+
+	public Logger getLogger()
+	{
+		return LoggerRegistry.getLogger(this.getName());
+	}
+
+	protected void actionWithLogger(Consumer<Logger> action)
+	{
+		Logger logger = getLogger();
+		if (logger != null)
+		{
+			action.accept(logger);
+		}
+		else
+		{
+			CarpetTISAdditionServer.LOGGER.warn("Failed to get carpet logger {}", this.getName());
+		}
+	}
+
+	public void log(Supplier<BaseText[]> messagePromise)
+	{
+		actionWithLogger(logger -> logger.log(messagePromise));
+	}
+
+	public void log(Logger.lMessage messagePromise)
+	{
+		actionWithLogger(logger -> logger.log(messagePromise));
+	}
+
+	public void log(Logger.lMessageIgnorePlayer messagePromise)
+	{
+		actionWithLogger(logger -> logger.log(messagePromise));
+	}
+
+	// Utils
+
+	public static String getCompoundOption(String... options)
+	{
+		return Joiner.on(OPTION_SEP).join(options);
 	}
 }
