bf46f9108a5e83b88945df8d2916234858f81a7e
Fallen_Breath <yqx791125833@gmail.com>
2022-05-04 12:19:02 +0800

    reformat

--- src/main/java/carpettisaddition/utils/IdentifierUtil.java
+++ src/main/java/carpettisaddition/utils/IdentifierUtil.java
@@ -9,8 +9,23 @@ import net.minecraft.util.registry.Registry;
 
 public class IdentifierUtil
 {
-	public static Identifier id(Block block) {return Registry.BLOCK.getId(block);}
-	public static Identifier id(Fluid fluid) {return Registry.FLUID.getId(fluid);}
-	public static Identifier id(EntityType<?> entityType) {return Registry.ENTITY_TYPE.getId(entityType);}
-	public static Identifier id(BlockEntityType<?> blockEntityType) {return Registry.BLOCK_ENTITY_TYPE.getId(blockEntityType);}
+	public static Identifier id(Block block)
+	{
+		return Registry.BLOCK.getId(block);
+	}
+
+	public static Identifier id(Fluid fluid)
+	{
+		return Registry.FLUID.getId(fluid);
+	}
+
+	public static Identifier id(EntityType<?> entityType)
+	{
+		return Registry.ENTITY_TYPE.getId(entityType);
+	}
+
+	public static Identifier id(BlockEntityType<?> blockEntityType)
+	{
+		return Registry.BLOCK_ENTITY_TYPE.getId(blockEntityType);
+	}
 }
