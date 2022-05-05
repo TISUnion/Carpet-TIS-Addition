51b910c00b9956b1d2b24ad1d9a03fa4b336738a
Fallen_Breath <yqx791125833@gmail.com>
2022-05-05 13:40:56 +0800

    22w18a-1.4.74 fabric carpet compat: carpet profiler mixin update

--- src/main/java/carpettisaddition/mixins/rule/synchronizedLightThread/CarpetProfilerMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/synchronizedLightThread/CarpetProfilerMixin.java
@@ -2,24 +2,25 @@ package carpettisaddition.mixins.rule.synchronizedLightThread;
 
 import carpet.utils.CarpetProfiler;
 import carpettisaddition.helpers.rule.synchronizedLightThread.LightThreadSynchronizer;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.Map;
 
 @Mixin(CarpetProfiler.class)
 public abstract class CarpetProfilerMixin
 {
-	@Mutable
-	@Shadow(remap = false) @Final private static String[] SECTIONS;
+	@Mutable @Final @Shadow(remap = false)
+	private static Map<String, String> SECTIONS;
 
 	static
 	{
-		List<String> list = Lists.newArrayList(SECTIONS);
-		list.add(LightThreadSynchronizer.SECTION_NAME);
-		SECTIONS = list.toArray(new String[0]);
+		Map<String, String> map = Maps.newLinkedHashMap(SECTIONS);
+		map.put(LightThreadSynchronizer.SECTION_NAME, "Light engine synchronization by rule synchronizedLightThread to make sure the lighting engine is not fall behind");
+		SECTIONS = Collections.unmodifiableMap(map);
 	}
 }
