9d6c8a4a150252ecd88738f815532ca76e7f371a
Fallen_Breath <yqx791125833@gmail.com>
2022-06-04 15:52:40 +0800

    carpet creativeNoClip enhanced: never gets detected by pressure plates or tripwires

---
+++ src/main/java/carpettisaddition/mixins/carpet/tweaks/rule/creativeNoClip/TripwireBlockMixin.java
@@ -0,0 +1,31 @@
+package carpettisaddition.mixins.carpet.tweaks.rule.creativeNoClip;
+
+import carpet.CarpetSettings;
+import carpettisaddition.helpers.carpet.tweaks.rule.creativeNoClip.CreativeNoClipHelper;
+import net.minecraft.block.TripwireBlock;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
+
+@Mixin(TripwireBlock.class)
+public abstract class TripwireBlockMixin
+{
+	@ModifyVariable(
+			method = "updatePowered",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/List;isEmpty()Z"
+			)
+	)
+	private List<? extends Entity> dontDetectCreativeNoClipPlayers(List<? extends Entity> entities)
+	{
+		if (CarpetSettings.creativeNoClip)
+		{
+			entities.removeIf(CreativeNoClipHelper::isNoClipPlayer);
+		}
+		return entities;
+	}
+}
