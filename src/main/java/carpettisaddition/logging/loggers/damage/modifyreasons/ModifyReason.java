fb50917a206b887ec14ae76bec4ca795db6ca8ca
Fallen_Breath <yqx791125833@gmail.com>
2022-07-17 10:09:47 +0800

    damage logger ModifyReason translation key cleaning up

--- src/main/java/carpettisaddition/logging/loggers/damage/modifyreasons/ModifyReason.java
+++ src/main/java/carpettisaddition/logging/loggers/damage/modifyreasons/ModifyReason.java
@@ -6,30 +6,25 @@ import net.minecraft.text.BaseText;
 
 public class ModifyReason extends TranslationContext
 {
-	public static final ModifyReason HELMET = new ModifyReason("Wearing a helmet");
-	public static final ModifyReason SHIELD = new ModifyReason("Holding a shield");
-	public static final ModifyReason RECENTLY_HIT = new ModifyReason("Recently hit");
-	public static final ModifyReason DIFFICULTY = new ModifyReason("Difficulty");
-	public static final ModifyReason IMMUNE = new ModifyReason("Immune to damage");
-	public static final ModifyReason INVULNERABLE = new ModifyReason("Invulnerable");
-	public static final ModifyReason RESPAWN_PROTECTION = new ModifyReason("Respawn protection");
-	public static final ModifyReason PVP_DISABLED = new ModifyReason("PVP disabled");
+	public static final ModifyReason HELMET = new ModifyReason("wearing_a_helmet");
+	public static final ModifyReason SHIELD = new ModifyReason("holding_a_shield");
+	public static final ModifyReason RECENTLY_HIT = new ModifyReason("recently_hit");
+	public static final ModifyReason DIFFICULTY = new ModifyReason("difficulty");
+	public static final ModifyReason IMMUNE = new ModifyReason("immuse_to_damage");
+	public static final ModifyReason INVULNERABLE = new ModifyReason("invulnerable");
+	public static final ModifyReason RESPAWN_PROTECTION = new ModifyReason("respawn_protection");
+	public static final ModifyReason PVP_DISABLED = new ModifyReason("pvp_disabled");
 
-	private final String name;
+	private final String translationName;
 
-	protected ModifyReason(String name)
+	protected ModifyReason(String translationName)
 	{
 		super(DamageLogger.getInstance().getTranslator());
-		this.name = name;
-	}
-
-	public String getName()
-	{
-		return name;
+		this.translationName = translationName;
 	}
 
 	public BaseText toText()
 	{
-		return tr("modify_reason." + this.getName().toLowerCase().replace(" ", "_"));
+		return tr("modify_reason." + this.translationName);
 	}
 }
