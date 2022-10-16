242dfdbb7dde43c57a5f290e120d1559e120b244
Fallen_Breath <yqx791125833@gmail.com>
2022-10-16 13:35:37 +0800

    increase source/target java version for higher mc versions
    so we can inject into static method in an interface (`carpettisaddition.mixins.command.lifetime.removal.InventoryOwnerMixin`)
    fixed 1.19+ InventoryOwner picking up item entity as item's removal reason

--- versions/1.19.x/src/main/java/carpettisaddition/mixins/command/lifetime/removal/InventoryOwnerMixin.java
+++ versions/1.19.x/src/main/java/carpettisaddition/mixins/command/lifetime/removal/InventoryOwnerMixin.java
@@ -1,26 +1,32 @@
 package carpettisaddition.mixins.command.lifetime.removal;
 
+import carpettisaddition.commands.lifetime.interfaces.LifetimeTrackerTarget;
+import carpettisaddition.commands.lifetime.removal.MobPickupRemovalReason;
 import carpettisaddition.utils.ModIds;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.entity.InventoryOwner;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.mob.MobEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Restriction(require = @Condition(value = ModIds.minecraft, versionPredicates = ">=1.19"))
 @Mixin(InventoryOwner.class)
 public interface InventoryOwnerMixin
 {
-	// OH NO, Mixin cannot inject into interface default method
-	// TODO: GO FIX THIS InventoryOwner picking up item entity as item's removal reason
-//	@Inject(
-//			method = "pickUpItem",
-//			at = @At(
-//					value = "INVOKE",
-//					target = "Lnet/minecraft/entity/ItemEntity;discard()V"
-//			)
-//	)
-//	static void onInventoryOwnerPickUpItemLifeTimeTracker(MobEntity mobEntity, InventoryOwner inventoryOwner, ItemEntity itemEntity, CallbackInfo ci)
-//	{
-//		((LifetimeTrackerTarget)itemEntity).recordRemoval(new MobPickupRemovalReason(mobEntity.getType()));
-//	}
+	// private static requires java9+
+	@Inject(
+			method = "pickUpItem",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/entity/ItemEntity;discard()V"
+			)
+	)
+	private static void onInventoryOwnerPickUpItemLifeTimeTracker(MobEntity mobEntity, InventoryOwner inventoryOwner, ItemEntity itemEntity, CallbackInfo ci)
+	{
+		((LifetimeTrackerTarget)itemEntity).recordRemoval(new MobPickupRemovalReason(mobEntity.getType()));
+	}
 }
