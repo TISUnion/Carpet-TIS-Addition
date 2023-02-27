ccb696209f03014c11088175eb6c24c110accadd
Fallen_Breath <yqx791125833@gmail.com>
2023-02-28 02:00:51 +0800

    github action update from mod template
    updated to 992a78bea8f18cbd8d38ed398e600120323d3d6b

---
+++ .github/workflows/scripts/summary.py
@@ -0,0 +1,34 @@
+import glob
+import json
+import os
+
+
+def read_prop(file_name: str, key: str) -> str:
+	with open(file_name) as prop:
+		return next(filter(
+			lambda l: l.split('=', 1)[0].strip() == key,
+			prop.readlines()
+		)).split('=', 1)[1].lstrip()
+
+
+target_subproject = os.environ.get('TARGET_SUBPROJECT', '')
+with open('.github/workflows/matrix_includes.json') as f:
+	matrix: list[dict] = json.load(f)
+
+with open(os.environ['GITHUB_STEP_SUMMARY'], 'w') as f:
+	f.write('## Build Artifacts Summary\n\n')
+	f.write('| Subproject | for Minecraft | Files |\n')
+	f.write('| --- | --- | --- |\n')
+
+	for m in matrix:
+		subproject = m['subproject_dir']
+		if target_subproject != '' and subproject != target_subproject:
+			continue
+		game_versions = read_prop('versions/{}/gradle.properties'.format(subproject), 'game_versions')
+		game_versions = game_versions.strip().replace('\\n', ', ')
+		file_names = glob.glob('build-artifacts/{}/build/libs/*.jar'.format(subproject))
+		file_names = ', '.join(map(
+			lambda fn: '`{}`'.format(os.path.basename(fn)),
+			filter(lambda fn: not fn.endswith('-sources.jar') and not fn.endswith('-dev.jar'), file_names)
+		))
+		f.write('| {} | {} | {} |\n'.format(subproject, game_versions, file_names))
