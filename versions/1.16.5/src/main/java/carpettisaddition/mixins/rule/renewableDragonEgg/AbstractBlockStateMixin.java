b87a3fd4ab4e518905c6abf99da7216a6e4ecd0f
Fallen_Breath <yqx791125833@gmail.com>
2023-04-06 02:37:16 +0800

    Fixed rule `renewableDragonEgg` does not work in mc1.19+
    resolved #96
    cherry-picked from 605297b4009006c681bbe7e7c9deb00df93803e6

---
+++ versions/1.16.5/src/main/java/carpettisaddition/mixins/rule/renewableDragonEgg/AbstractBlockStateMixin.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the Carpet TIS Addition project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2023  Fallen_Breath and contributors
+ *
+ * Carpet TIS Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpet TIS Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpet TIS Addition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package carpettisaddition.mixins.rule.renewableDragonEgg;
+
+import carpettisaddition.CarpetTISAdditionSettings;
+import carpettisaddition.utils.ModIds;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Lithium for mc1.16+, or Minecraft 1.19+, sets up immutable hasRandomTicks cache,
+ * and the cache may not match the actual value,
+ * since whether dragon egg block has random tick is changeable
+ * So, if the block is dragon egg, use our own carpet setting rule value
+ */
+@Restriction(require = @Condition(value = ModIds.minecraft, versionPredicates = ">=1.16"))
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public abstract class AbstractBlockStateMixin
+{
+	@Shadow
+	public abstract Block getBlock();
+
+	/**
+	 * Lithium block.flatten_states sets up immutable hasRandomTicks cache, and the cache may not match the actual value
+	 * since whether dragon egg block has random tick is changeable
+	 * So, if rule renewableDragonEgg is enabled, change the return value to true
+	 */
+	@Inject(method = "hasRandomTicks", at = @At("HEAD"), cancellable = true)
+	private void hasRandomTicksDragonEgg(CallbackInfoReturnable<Boolean> cir)
+	{
+		if (CarpetTISAdditionSettings.renewableDragonEgg)
+		{
+			if (this.getBlock() == Blocks.DRAGON_EGG)
+			{
+				cir.setReturnValue(true);
+				cir.cancel();
+			}
+		}
+	}
+}
