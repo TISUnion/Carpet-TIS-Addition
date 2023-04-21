6c7df28de642322c7f99e7eb6a888d258b37f552
Fallen_Breath <yqx791125833@gmail.com>
2023-04-21 10:48:49 +0800

    23w16a

--- src/main/java/carpettisaddition/mixins/rule/syncServerMsptMetricsData/DebugHudMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/syncServerMsptMetricsData/DebugHudMixin.java
@@ -32,7 +32,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-//#if MC >= 11600
+//#if MC >= 12000
+//$$ import net.minecraft.client.gui.DrawableHelper;
+//#elseif MC >= 11600
 //$$ import net.minecraft.client.util.math.MatrixStack;
 //#endif
 
@@ -40,7 +42,9 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public abstract class DebugHudMixin
 {
 	@Shadow protected abstract void drawMetricsData(
-			//#if MC >= 11600
+			//#if MC >= 12000
+			//$$ DrawableHelper drawableHelper,
+			//#elseif MC >= 11600
 			//$$ MatrixStack matrixStack,
 			//#endif
 			MetricsData metricsData, int startY, int firstSample, boolean isClient
@@ -56,8 +60,10 @@ public abstract class DebugHudMixin
 			locals = LocalCapture.CAPTURE_FAILHARD
 	)
 	private void syncServerMsptMetricsData_drawIfPossible(
-			//#if MC >= 11600
-			//$$ MatrixStack matrixStack,
+			//#if MC >= 12000
+			//$$ DrawableHelper arg0,
+			//#elseif MC >= 11600
+			//$$ MatrixStack arg0,
 			//#endif
 			CallbackInfo ci,
 			Entity entity, int i, IntegratedServer integratedServer
@@ -72,7 +78,7 @@ public abstract class DebugHudMixin
 				// vanilla copy
 				this.drawMetricsData(
 						//#if MC >= 11600
-						//$$ matrixStack,
+						//$$ arg0,
 						//#endif
 						metricsData, i - Math.min(i / 2, 240), i / 2, false
 				);
