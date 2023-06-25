3ece58a546247232942c1b97e24ea9fa47de258f
Fallen_Breath <yqx791125833@gmail.com>
2023-06-26 01:27:38 +0800

    using mixin-extras: `@Redirect` -> `@ModifyExpressionValue` for rule `enchantCommandNoRestriction`

--- src/main/java/carpettisaddition/mixins/rule/enchantCommandNoRestriction/EnchantCommandMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/enchantCommandNoRestriction/EnchantCommandMixin.java
@@ -21,13 +21,12 @@
 package carpettisaddition.mixins.rule.enchantCommandNoRestriction;
 
 import carpettisaddition.CarpetTISAdditionSettings;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.command.EnchantCommand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -35,40 +34,36 @@ import java.util.Collections;
 @Mixin(value = EnchantCommand.class, priority = 900)
 public abstract class EnchantCommandMixin
 {
-	@Redirect(
+	@ModifyExpressionValue(
 			method = "execute",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/enchantment/Enchantment;getMaximumLevel()I"
-			),
-			require = 0  // for potential mod compatibility
+			)
 	)
-	private static int removeLevelRestriction(Enchantment enchantment)
+	private static int enchantCommandNoRestriction_removeLevelRestriction(int maxLevel)
 	{
 		if (CarpetTISAdditionSettings.enchantCommandNoRestriction)
 		{
-			return Integer.MAX_VALUE;
+			maxLevel = Integer.MAX_VALUE;
 		}
-		// vanilla copy
-		return enchantment.getMaximumLevel();
+		return maxLevel;
 	}
 
-	@Redirect(
+	@ModifyExpressionValue(
 			method = "execute",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/enchantment/Enchantment;isAcceptableItem(Lnet/minecraft/item/ItemStack;)Z"
-			),
-			require = 0  // for potential mod compatibility
+			)
 	)
-	private static boolean removeAcceptableCheck(Enchantment enchantment, ItemStack stack)
+	private static boolean enchantCommandNoRestriction_removeAcceptableCheck(boolean isAcceptable)
 	{
 		if (CarpetTISAdditionSettings.enchantCommandNoRestriction)
 		{
-			return true;
+			isAcceptable = true;
 		}
-		// vanilla copy
-		return enchantment.isAcceptableItem(stack);
+		return isAcceptable;
 	}
 
 	@ModifyArg(
@@ -79,7 +74,7 @@ public abstract class EnchantCommandMixin
 			),
 			index = 0
 	)
-	private static Collection<Enchantment> removeExistingCheck(Collection<Enchantment> enchantments)
+	private static Collection<Enchantment> enchantCommandNoRestriction_removeAlreadyExistingCheck(Collection<Enchantment> enchantments)
 	{
 		if (CarpetTISAdditionSettings.enchantCommandNoRestriction)
 		{
