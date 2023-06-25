dcbfd80dabebc1efe411877e8e1e3e7e3ea9f65a
Fallen_Breath <yqx791125833@gmail.com>
2023-06-26 01:25:20 +0800

    using mixin-extras: `@Redirect` -> `@ModifyArg` for rule `dispenserNoItemCost`

--- src/main/java/carpettisaddition/mixins/rule/dispenserNoItemCost/ItemDispenserBehaviorMixin.java
+++ src/main/java/carpettisaddition/mixins/rule/dispenserNoItemCost/ItemDispenserBehaviorMixin.java
@@ -23,35 +23,26 @@ package carpettisaddition.mixins.rule.dispenserNoItemCost;
 import carpettisaddition.CarpetTISAdditionSettings;
 import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPointer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ItemDispenserBehavior.class)
 public abstract class ItemDispenserBehaviorMixin
 {
-	@Shadow
-	protected abstract ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack);
-
-	@Redirect(
+	@ModifyArg(
 			method = "dispense",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/block/dispenser/ItemDispenserBehavior;dispenseSilently(Lnet/minecraft/util/math/BlockPointer;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"
 			)
 	)
-	private ItemStack injectNoCostBehavior(ItemDispenserBehavior itemDispenserBehavior, BlockPointer pointer, ItemStack stack)
+	private ItemStack dispenserNoItemCost_useACopySoItCostNothing(ItemStack stack)
 	{
 		if (CarpetTISAdditionSettings.dispenserNoItemCost)
 		{
-			this.dispenseSilently(pointer, stack.copy());
-			return stack;
+			stack = stack.copy();
 		}
-		else
-		{
-			return this.dispenseSilently(pointer, stack);
-		}
+		return stack;
 	}
 }
