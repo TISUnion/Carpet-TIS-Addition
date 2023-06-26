2f81e02c8b1117591eb50930292a33f3c4761d38
Fallen_Breath <yqx791125833@gmail.com>
2023-06-27 02:01:20 +0800

    using mixin-extras: `@Redirect` -> `@WrapOperation` for rule `yeetUpdateSuppressionCrash` SixWayEntry mixin

--- versions/1.19.2/src/main/java/carpettisaddition/mixins/rule/yeetUpdateSuppressionCrash/ChainRestrictedNeighborUpdaterSixWayEntryMixin.java
+++ versions/1.19.2/src/main/java/carpettisaddition/mixins/rule/yeetUpdateSuppressionCrash/ChainRestrictedNeighborUpdaterSixWayEntryMixin.java
@@ -22,36 +22,35 @@ package carpettisaddition.mixins.rule.yeetUpdateSuppressionCrash;
 
 import carpettisaddition.CarpetTISAdditionSettings;
 import carpettisaddition.helpers.rule.yeetUpdateSuppressionCrash.UpdateSuppressionException;
-import carpettisaddition.utils.mixin.testers.YeetUpdateSuppressionCrashTester;
-import me.fallenbreath.conditionalmixin.api.annotation.Condition;
-import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Restriction(require = @Condition(type = Condition.Type.TESTER, tester = YeetUpdateSuppressionCrashTester.class))
 @Mixin(targets = "net.minecraft.world.block.ChainRestrictedNeighborUpdater$SixWayEntry")
 public abstract class ChainRestrictedNeighborUpdaterSixWayEntryMixin
 {
-	@SuppressWarnings("deprecation")
-	@Redirect(
+	@WrapOperation(
 			method = "update",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/block/BlockState;neighborUpdate(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;Lnet/minecraft/util/math/BlockPos;Z)V"
 			)
 	)
-	private void yeetUpdateSuppressionCrash_implOnSixWayEntryUpdate(BlockState instance, World world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean bl)
+	private void yeetUpdateSuppressionCrash_implOnSixWayEntryUpdate(
+			BlockState instance, World world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean notify,
+			Operation<Void> original
+	)
 	{
 		if (CarpetTISAdditionSettings.yeetUpdateSuppressionCrash)
 		{
 			try
 			{
-				instance.neighborUpdate(world, pos, neighborBlock, neighborPos, bl);
+				original.call(instance, world, pos, neighborBlock, neighborPos, notify);
 			}
 			catch (Throwable throwable)
 			{
@@ -63,7 +62,7 @@ public abstract class ChainRestrictedNeighborUpdaterSixWayEntryMixin
 		else
 		{
 			// vanilla
-			instance.neighborUpdate(world, pos, neighborBlock, neighborPos, bl);
+			original.call(instance, world, pos, neighborBlock, neighborPos, notify);
 		}
 	}
 }
