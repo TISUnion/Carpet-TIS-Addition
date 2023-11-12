3eaea3b55a9904834413ad577fb01331b97ecffc
Fallen_Breath <yqx791125833@gmail.com>
2023-11-12 23:28:08 +0800

    Fix clientside custom payload packet memoryleak for TISCM protocol
    https://bugs.mojang.com/browse/MC-121884, for TISCM packets

--- src/main/java/carpettisaddition/mixins/network/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/carpettisaddition/mixins/network/ClientPlayNetworkHandlerMixin.java
@@ -27,6 +27,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -88,9 +89,18 @@ public abstract class ClientPlayNetworkHandlerMixin
 		Identifier channel = ((CustomPayloadS2CPacketAccessor) packet).getChannel();
 		if (TISCMCustomPayload.ID.equals(channel))
 		{
-			TISCMCustomPayload tiscmCustomPayload = new TISCMCustomPayload(((CustomPayloadS2CPacketAccessor)packet).getData());
-			TISCMClientPacketHandler.getInstance().dispatch((ClientPlayNetworkHandler)(Object)this, tiscmCustomPayload);
-			ci.cancel();
+			PacketByteBuf packetByteBuf = ((CustomPayloadS2CPacketAccessor)packet).getData();
+			try
+			{
+				TISCMCustomPayload tiscmCustomPayload = new TISCMCustomPayload(packetByteBuf);
+				TISCMClientPacketHandler.getInstance().dispatch((ClientPlayNetworkHandler)(Object)this, tiscmCustomPayload);
+				ci.cancel();
+			}
+			finally
+			{
+				// Fix https://bugs.mojang.com/browse/MC-121884, for TISCM packets
+				packetByteBuf.release();
+			}
 		}
 		//#endif
 	}
